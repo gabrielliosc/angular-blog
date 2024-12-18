1a01897ec80a983b67b4b7f0268c238e30a5a875
Gabrielli Oliveira <gabrielli.osc@gmail.com>
2024-12-18 00:13:16 -0300

    Finish blog exercise

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Hábitos Atômicos",
+    "description": "Não importa quais sejam seus objetivos, Hábitos Atômicos oferece um método eficaz para você se aprimorar ― todos os dias. James Clear, um dos mais expoentes especialistas na criação de hábitos, revela as estratégicas práticas que o ensinarão, exatamente, como criar bons hábitos, abandonar os maus e fazer pequenas mudanças de comportamento que levam a resultados impressionantes.",
+    "photoCover":"https://m.media-amazon.com/images/I/81eT2pjx4jL._SY342_.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "O amor não tem nome",
+    "description": "Lanie Bloom está arrasando. Ela tem uma carreira promissora como editora de livros e está noiva de um cara que atende todas as noventa e nove características cuidadosamente selecionadas de sua lista do homem perfeito.",
+    "photoCover":"https://m.media-amazon.com/images/I/71zcu-Ke0IL._SY342_.jpg"
+  },
+  {
+    "id":"3",
+    "title": "A Biblioteca da Meia-Noite",
+    "description": "Aos 35 anos, Nora Seed é uma mulher cheia de talentos e poucas conquistas. Arrependida das escolhas que fez no passado, ela vive se perguntando o que poderia ter acontecido caso tivesse vivido de maneira diferente. Após ser demitida e seu gato ser atropelado, Nora vê pouco sentido em sua existência e decide colocar um ponto final em tudo. Porém, quando se vê na Biblioteca da Meia-Noite, Nora ganha uma oportunidade única de viver todas as vidas que poderia ter vivido.",
+    "photoCover":"https://m.media-amazon.com/images/I/81iqH8dpjuL._SY342_.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Ainda estou aqui",
+    "description": "Eunice Paiva é uma mulher de muitas vidas. Casada com o deputado Rubens Paiva, esteve ao seu lado quando foi cassado e exilado, em 1964. Mãe de cinco filhos, passou a criá-los sozinha quando, em 1971, o marido foi preso por agentes da ditadura, a seguir torturado e morto. Em meio à dor, ela se reinventou. Voltou a estudar, tornou-se advogada, defensora dos direitos indígenas. Nunca chorou na frente das câmeras.",
+    "photoCover":"https://m.media-amazon.com/images/I/715h8DWokhL._SY342_.jpg"
   }
 ]
